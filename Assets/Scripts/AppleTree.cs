7fe59bed7205097dd844bb338a6840c86051b29d
Alex Ivanov <oleksii120@gmail.com>
2023-11-18 11:26:10 +0200

    Added Apple tree movement script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    // Apple template
+    [Header("Set in inspector")] public GameObject applePrefab;
+
+    public float speed = 1;
+
+    public float leftAndRightEdge = 10;
+    
+    public float chanceToChangeDirections = 0.1f;
+    
+    public float secondsBetweenAppleDrops = 1;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+            speed = Mathf.Abs(speed); // Move right
+        else if (pos.x > leftAndRightEdge)
+            speed = -Mathf.Abs(speed); // Move left
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+            speed *= -1; // Change direction
     }
 }
